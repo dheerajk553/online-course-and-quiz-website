222425867af37ba015c080d001f85f5f794a26d8
Dheeraj Kumar <270647844@yoobeestudent.ac.nz>
2025-10-10 13:12:41 +1300

    Initial Django project setup with Course and Lesson models

---
+++ courses/models.py
@@ -0,0 +1,20 @@
+
+# Create your models here.
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
