f1b2b51e8c035d1d4ac1cf206a9cba8bed435c64
Dheeraj Kumar <270647844@yoobeestudent.ac.nz>
2025-10-26 17:27:09 +1300

    Initial commit

--- online_course_and_quiz_website/settings.py
+++ online_course_and_quiz_website/settings.py
@@ -129,8 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / 'courses' / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
