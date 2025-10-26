f33ee90c2e8e352c5fc72b73c32f68bfa002ec2c
Dheeraj Kumar <270647844@yoobeestudent.ac.nz>
2025-10-27 07:38:50 +1300

    Update settings.py for localhost and deployment

--- online_course_and_quiz_website/settings.py
+++ online_course_and_quiz_website/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-ur8$2%47=-h^t=to3@kpwc))#^#tp0y1*v2mt^_t6ty1#87lbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com', 'dheerajk553.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'Dheerajkumar.pythonanywhere.com']
+
 
 # Application definition
 
